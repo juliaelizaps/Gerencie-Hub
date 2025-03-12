d2a840d65b8e7f58950385af3a64de0814198b05
Julia Eliza <juliaelizaps@gmail.com>
2025-03-11 23:00:37 -0300

    feat: creates Mobile NavBar

--- src/app/components/navigation/MobileNav.tsx
+++ src/app/components/navigation/MobileNav.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
+import { VscClose } from "react-icons/vsc";
 
 const MobileNav = () => {
-  return (
-    <div> MobileNav </div>
-  )
+    return (
+        <div className='fixed right-0 transition-all duration-500 left-0 bottom-0 h-[100vh] bg-[#000000e0] z-[1002]'>
+            <VscClose className='text-[4px] absolute top-[2rem]  right-[2rem] w-[2rem] h-[2rem] text-white' />
+            {/* Nav div */}
+            <div className=' bg-linear-to-b from-red-950 from-40% to-black transition-all-500 delay-200 text-white flex flex-col items-center justify-center w-[70%] h-[100%]'>
+                {/* Nav links */}
+                <ul className='space-y-10'>
+                    <li className='text-[35px] font-medium hover:text-orange-500 transition-all duration-200 text-white'>
+                        <Link href="/">Hub</Link>
+                    </li>
+                    <li className='text-[35px] font-medium hover:text-orange-500 transition-all duration-200 text-white'>
+                        <Link href="/">Produtos</Link>
+                    </li>
+                    <li className='text-[35px] font-medium hover:text-orange-500 transition-all duration-200 text-white'>
+                        <Link href="/">Pedidos</Link>
+                    </li>
+                    <li className='text-[35px] font-medium  hover:text-orange-500 transition-all duration-200 text-white'>
+                        <Link href="/">Lista de Pedidos</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default MobileNav
--- src/app/components/navigation/ResponsiveNav.tsx
+++ src/app/components/navigation/ResponsiveNav.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
 
 const ResponsiveNav = () => {
+    // const [showNav, setShowNav]=useState(false);
+    // const openNavhandler=()=>setShowNav(true);
+    // const closeNavHandler=()=>setShowNav(false);
   return (
     <div>
-      <Nav/>
+      {/* <Nav openNav={openNavhandler}/>
+      <MobileNav showNav={setShowNav} closeNav={closeNavHandler}/> */}
+      <Nav />
       <MobileNav/>
     </div>
   )
